d914da021dc78bc911cec7bde741e1acd5fb262a
makemile <mkengya@gmail.com>
2021-09-16 00:51:01 -0500

    saveDev

--- src/index.js
+++ src/index.js
@@ -1,35 +1,34 @@
-const { error } = require('console');
 const fs = require('fs'); //nos devuelve un objeto, lo almacenamos dentro de una constante//
 const path = require('path');
 
 //function readFile//
 
-// fs.readFile('../README.md','utf-8',(error, data) =>{
-//     if (!error){
-//      console.log(data);
-//    }else{
-//    console.log(`error: ${error}`);
-// }
-// })
+ fs.readFile('../README.md','utf-8',(error, data) =>{
+   if (!error){
+      console.log(data);
+    }else{
+    console.log(`error: ${error}`);
+ }
+ })
 
 
-// //functions extensions name//
-// let extName = path.extname('/README.md');
-// console.log(extName);
+//functions extensions name//
+ let extName = path.extname('/README.md');
+   console.log(extName);
 
-// //Comparing extensions from a list of file paths -example 1--//
-// let path_Array = [
-//     "/c/users/KENGYA/Documents/Develop/LIM015-md-links/README.md",
-//     "/c/users/KENGYA/Documents/Develop/LIM015-md-links/hola.txt"
-// ];
+ //Comparing extensions from a list of file paths -example 1--//
+ let path_Array = [
+     "/c/users/KENGYA/Documents/Develop/LIM015-md-links/README.md",
+     "/c/users/KENGYA/Documents/Develop/LIM015-md-links/hola.txt"
+ ];
 
-// path_Array.forEach(filePath => {
-//     if(path.extname(filePath) == '.txt') {
-//         console.log(filePath);
-//   }
-// });
+ path_Array.forEach(filePath => {
+     if(path.extname(filePath) == '.txt') {
+         console.log(filePath);
+   }
+ });
 
-// 
+ //function readddir//
 fs.readdir('../src', (error, files) => {
     if (error){
         throw error
