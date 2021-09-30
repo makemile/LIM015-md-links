a6c8e02a20d279bceccd1b8c7e75896bbcd993c6
makemile <mkengya@gmail.com>
2021-09-29 23:29:05 -0500

    pasaron con éxito los test de http

--- src/mdlinks.js
+++ src/mdlinks.js
@@ -1,23 +1,28 @@
 const functions = require('../src/Api');
 
-const mdlinks = (path, options = {validate: false}) => {
-    return new Promise ((resolve, reject) => {
+const mdlinks = (path, options = {}) => {
+    return new Promise ( (resolve, reject) => {
         //validar que el path existe//
         if(!functions.existsPath(path)) {
+            const err = 'invalid path';
+            resolve(err)
             reject('la ruta no existe, es invalida')
             //extraer c/u de los links//
         }else{
-            const viewLinks = functions.searchLinks(path);
-            if(!(options.validate)){
-                resolve (viewLinks);
+            if(options.validate){
+               resolve (functions.validateLinks(functions.searchLinks(path)))
             }
            else{
-               const statLinks = functions.validateLinks(viewLinks);
-               statLinks.then((res) => resolve(res));
+               resolve(functions.searchLinks(path));
            }
         }
     });
  };
 
- mdlinks('../lib').then((res) => console.log(res,40));
+mdlinks('C:\\Users\\KENGYA\\Documents\\Develop\\LIM015-md-links\\lib\\linkroto.md', {validate : false}).then((res) => { //crear una promesa global//
+    console.log(res,23);
+   })
+   .catch((error) =>{
+    console.log(error);
+    }); //map debo construir un array de prom
     module.exports = {mdlinks};
